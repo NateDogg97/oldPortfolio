c3d1f7496db83f576a729dd37430a6af6848131a
NateDogg <natmays97@gmail.com>
2023-06-03 15:04:19 -0500

    fixed a bug with the loading-image

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,26 +1,28 @@
+window.addEventListener("DOMContentLoaded", function() {
+    let options = {
+        root: null, 
+        rootMargin: '50px',
+        threshold: 0
+    };
+
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                let iframe = entry.target;
+                iframe.src = iframe.getAttribute('data-src');
+                observer.unobserve(iframe);
+            }
+        });
+    }, options);
+
+    observer.observe(document.querySelector('#iframe'));
+}, false);
+
 function loadIframe() {
-    document.getElementById('loading-image').style.display = 'none';
-    document.getElementById('iframe').style.visibility = 'visible';
+    let iframe = document.getElementById('iframe');
+    let loadingImage = document.getElementById('loading-image');
+    if(iframe.src !== '') {
+        loadingImage.style.display = 'none';
+        iframe.style.visibility = 'visible';
+    }
 }
-
-let options = {
-    root: null, 
-    rootMargin: '50px',
-    threshold: 0
-};
-
-let observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            let iframe = entry.target;
-            iframe.src = iframe.getAttribute('data-src');
-            observer.unobserve(iframe);
-        }
-    });
-}, options);
-
-console.log("hello");
-
-observer.observe(document.querySelector('#iframe'));
-
-
